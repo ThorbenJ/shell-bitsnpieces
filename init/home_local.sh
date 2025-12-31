408b75d4e6fafdc558188c75be55cab9df621d8f
Thorben Jändling <thorbenj@users.noreply.github.com>
2025-12-31 10:14:22 +0400

    better ~/Local setup

--- init/home_local.sh
+++ init/home_local.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+prepend_path() {
+    local varname="$1"
+    local newpath="$2"
+    local oldvalue="${!varname}"
+
+    # Dir doesn't exist
+    [[ -d "$newpath" ]] || return
+
+    # Aleady in path
+    [[ ":$oldvalue:" == *":$newpath:"* ]] && return
+
+    if [[ -z "$oldvalue" ]]; then
+        export "$varname"="$newpath"
+    else
+        export "$varname"="$newpath:$oldvalue"
+    fi
+}
+
+if [[ -z "$MANPATH" ]]; then
+    #trailing colon should cause man to auto include system paths
+    export MANPATH=":"
+fi
+
 # Setup ~/Local as the place to find things
-
-
-PERL5LIB=$HOME/Local/lib/perl5:$PERL5LIB
-GOPATH=$HOME/Local/lib/go
-PATH=$HOME/Local/bin:$PATH
-
-export PERL5LIB GOPATH PATH
+prepend_path PATH              "$HOME/Local/bin"
+prepend_path PERL5LIB          "$HOME/Local/lib/perl5"
+prepend_path GOPATH            "$HOME/Local/lib/go"
+prepend_path MANPATH           "$HOME/Local/share/man"
+prepend_path LD_LIBRARY_PATH   "$HOME/Local/lib"
+prepend_path PKG_CONFIG_PATH   "$HOME/Local/lib/pkgconfig"
+prepend_path PYTHONPATH        "$HOME/Local/lib/python"
